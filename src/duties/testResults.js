6236f85ca04834cdafd3a45bbeb99939358e5e80
Sean Clayton <sclayton@smartprocure.us>
2017-08-04 13:00:06 -0400

    Show test failures in comment

--- src/duties/testResults.js
+++ src/duties/testResults.js
@@ -1,9 +1,21 @@
-let { log } = require('../utils')
+let _ = require('lodash/fp')
+let { log, stripAnsi, fileNameFromPath } = require('../utils')
 
 let hasTestErrors = ({ testResults, fail }) => {
   if (testResults && testResults.numFailedTests > 0) {
+    let allFailures = _.flow(
+      _.filter(result => !_.isEmpty(result.message)),
+      _.map(
+        r => `<details>
+  <summary>${fileNameFromPath(r.name)}</summary>
+  ${stripAnsi(r.message)}
+</details>`,
+      ),
+      _.join('\n'),
+    )(testResults.testResults)
+
     log(fail)(
-      'This PR has failing tests. Please alleviate the errors and commit them',
+      `This PR has failing tests. Please alleviate the errors and commit them\n\n${allFailures}`,
     )
   }
 }
