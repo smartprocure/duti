caa6674b0653263c13377032b13a0ce4cd597822
Sean Clayton <clayton.sean@gmail.com>
2017-08-04 00:08:28 -0400

    ls -> duti

--- bin/duti.js
+++ bin/duti.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 
 let { spawn } = require('child_process')
 let path = require('path')
-let ls = spawn('danger', [
+let duti = spawn('danger', [
   'run',
   '--dangerfile',
   path.resolve(__dirname, '../src/dangerfile.js'),
 ])
 
-ls.stdout.on('data', data => {
+duti.stdout.on('data', data => {
   console.log(`${data}`)
 })
 
-ls.stderr.on('data', data => {
+duti.stderr.on('data', data => {
   console.log(`${data}`)
 })
