d39c3235d86ad46fd5ba92080f299b0c2805bb68
Sean Clayton <sclayton@smartprocure.us>
2017-08-04 15:27:03 -0400

    Pretty up template strings

--- src/duties/lint.js
+++ src/duties/lint.js
@@ -1,19 +1,24 @@
 let _ = require('lodash/fp')
 let { log, fileNameFromPath } = require('../utils')
 
+let messageTemplate = message => `
+<li>
+  <strong>${message.message}</strong>
+  <br />
+  <code>Line ${message.line}: ${message.source.trim()}</code>
+</li>
+`
+
+let lintTemplate = (lint, severity) => `
+<details>
+  <summary>${fileNameFromPath(lint.filePath)}</summary>
+  <ul>${formatLintMessages(
+    _.filter(m => m.severity === severity, lint.messages),
+  )}</ul>
+</details>`
+
 let formatLintMessages = messages =>
-  _.flow(
-    _.map(
-      message => `
-  <li>
-    <strong>${message.message}</strong>
-    <br />
-    <code>Line ${message.line}: ${message.source.trim()}</code>
-  </li>
-`,
-    ),
-    _.join('\n'),
-  )(messages)
+  _.flow(_.map(messageTemplate), _.join('\n'))(messages)
 
 let formatLint = (lintRes, severity) =>
   _.flow(
@@ -22,15 +27,7 @@ let formatLint = (lintRes, severity) =>
         !_.isEmpty(lint.messages) &&
         _.some(m => m.severity === severity, lint.messages),
     ),
-    _.map(
-      lint => `
-  <details>
-    <summary>${fileNameFromPath(lint.filePath)}</summary>
-    <ul>${formatLintMessages(
-      _.filter(m => m.severity === severity, lint.messages),
-    )}</ul>
-  </details>`,
-    ),
+    _.map(lint => lintTemplate(lint, severity)),
     _.join('\n'),
   )(lintRes)
 
--- src/duties/testResults.js
+++ src/duties/testResults.js
@@ -1,16 +1,16 @@
 let _ = require('lodash/fp')
 let { log, stripAnsi, fileNameFromPath } = require('../utils')
 
+let resultTemplate = r => `<details>
+  <summary>${fileNameFromPath(r.name)}</summary>
+  ${stripAnsi(r.message)}
+</details>`
+
 let hasTestErrors = ({ testResults, fail }) => {
   if (testResults && testResults.numFailedTests > 0) {
     let allFailures = _.flow(
       _.filter(result => !_.isEmpty(result.message)),
-      _.map(
-        r => `<details>
-  <summary>${fileNameFromPath(r.name)}</summary>
-  ${stripAnsi(r.message)}
-</details>`,
-      ),
+      _.map(resultTemplate),
       _.join('\n'),
     )(testResults.testResults)
 
