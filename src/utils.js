2f9da8cecc326a1305d73f1fd288a266bde71eeb
Sean Clayton <sclayton@smartprocure.us>
2017-08-07 19:08:00 -0400

    Refactor json file reading

--- src/utils.js
+++ src/utils.js
@@ -24,25 +24,24 @@ let fileToJson = F.unless(_.isNil, JSON.parse)
 let getRunningDirectory = async () => path.dirname(await pkgup())
 
 let readJsonFile = async filePath =>
-  fileToJson(await readFileIfExists(path.resolve(filePath)))
+  fileToJson(await readFileIfExists(filePath))
 
-let getLintResults = async ({ config: { lintResultsPath } }) =>
-  readJsonFile(
-    path.resolve(
-      await getRunningDirectory(),
-      lintResultsPath,
-      'lint-results.json',
+let readLocalJsonFile = file => async dir =>
+  fileToJson(
+    await readFileIfExists(
+      path.resolve(await getRunningDirectory(), dir, file),
     ),
   )
 
-let getTestResults = async ({ config: { testResultsPath } }) =>
-  readJsonFile(
-    path.resolve(
-      await getRunningDirectory(),
-      testResultsPath,
-      'test-results.json',
-    ),
-  )
+let getLintResults = _.flow(
+  _.get('config.lintResultsPath'),
+  readLocalJsonFile('lint-results.json'),
+)
+
+let getTestResults = _.flow(
+  _.get('config.testResultsPath'),
+  readLocalJsonFile('test-results.json'),
+)
 
 module.exports = {
   log,
