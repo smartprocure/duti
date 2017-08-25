e0e61cbdc7b2710d97d8cabced8d30eeb823e315
Sean Clayton <clayton.sean@gmail.com>
2017-08-25 16:55:15 -0400

    Force push?

--- src/duties/autoFix.js
+++ src/duties/autoFix.js
@@ -16,7 +16,7 @@ let autoFix = async ({ message }) => {
 
     if (hasModifiedFiles) {
       execSync(
-        'git commit -am "Automagically formatted by Duti!\n\nhttps://github.com/smartprocure/duti" && git push',
+        'git commit -am "Automagically formatted by Duti!\n\nhttps://github.com/smartprocure/duti" && git push --force',
       )
       message(
         'We were able to automatically fix some formatting issues in this PR for you!',
