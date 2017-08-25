a2adb7dd26a7a036be53df97e1d5282c0cfe7bf6
Sean Clayton <clayton.sean@gmail.com>
2017-08-25 15:44:41 -0400

    duti:fmt

--- src/duties/index.js
+++ src/duties/index.js
@@ -2,5 +2,6 @@ let pr = require('./pr')
 let lint = require('./lint')
 let testResults = require('./testResults')
 let library = require('./library')
+let autoFix = require('./autoFix')
 
-module.exports = Object.assign({}, pr, lint, testResults, library)
+module.exports = Object.assign({}, pr, lint, testResults, library, autoFix)
