b26e65994be1c5da690adde5611b30f1eeb88ca9
Decrapifier <drodriguez@smartprocure.us>
2017-11-01 19:41:39 +0000

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti

--- src/duties/browserResults.js
+++ src/duties/browserResults.js
@@ -1,18 +1,18 @@
-let _ = require("lodash/fp");
+let _ = require('lodash/fp')
 
 let hasBrowserErrors = ({ browserResults, fail, message }) => {
   if (_.isNil(browserResults)) {
-    message("Could not find any browser results.");
-    return;
+    message('Could not find any browser results.')
+    return
   }
 
-  let { result, summary } = browserResults;
+  let { result, summary } = browserResults
 
   if (result && summary) {
-    let { error, failed, exitCode } = summary;
+    let { error, failed, exitCode } = summary
     if (error === false && failed === 0 && exitCode === 0) {
-      message("Your PR has no browser errors. Great job!");
-      return;
+      message('Your PR has no browser errors. Great job!')
+      return
     }
 
     if (failed > 0) {
@@ -27,21 +27,21 @@ let hasBrowserErrors = ({ browserResults, fail, message }) => {
           f => `\nSuite: ${f.suite}\nDescription: ${f.description}\n${f.log}\n`
         ),
         _.concat([`Your PR has ${failed} failed browser tests:`]),
-        _.join("\n"),
+        _.join('\n'),
         fail
-      )(result);
-      return;
+      )(result)
+      return
     }
 
     // All other cases: error is not false, exitCode is not 0 etc.
-    fail("Browser test error. Please see CI logs for more details.");
+    fail('Browser test error. Please see CI logs for more details.')
   } else {
     message(
-      "Incorrect browser result format. Please see CI logs for more details."
-    );
+      'Incorrect browser result format. Please see CI logs for more details.'
+    )
   }
-};
+}
 
 module.exports = {
-  hasBrowserErrors: hasBrowserErrors
-};
+  hasBrowserErrors: hasBrowserErrors,
+}
--- src/duties/lint.js
+++ src/duties/lint.js
@@ -1,13 +1,13 @@
-let _ = require("lodash/fp");
-let { basename } = require("path");
+let _ = require('lodash/fp')
+let { basename } = require('path')
 
 let messageTemplate = message => `
   <strong>${message.message}</strong>
   <code>Line ${message.line}${message.source
   ? `: ${message.source.trim()}`
-  : ""}</code>`;
+  : ''}</code>`
 
-let formatLintMessages = _.flow(_.map(messageTemplate), _.join("\n"));
+let formatLintMessages = _.flow(_.map(messageTemplate), _.join('\n'))
 
 let lintTemplate = severity => lint => `
 <details>
@@ -16,53 +16,53 @@ let lintTemplate = severity => lint => `
   ${formatLintMessages(
     severity ? _.filter({ severity }, lint.messages) : lint.messages
   )}
-</details>`;
+</details>`
 
 let formatLint = severity =>
   _.flow(
     _.filter(lint => _.some({ severity }, lint.messages)),
     _.map(lintTemplate(severity)),
-    _.join("")
-  );
+    _.join('')
+  )
 
-let formatStandardJSLint = _.flow(_.map(lintTemplate()), _.join(""));
+let formatStandardJSLint = _.flow(_.map(lintTemplate()), _.join(''))
 
 let hasLintErrors = ({ lintResults, fail, message }) => {
   if (_.isNil(lintResults)) {
-    message("Could not find any lint results");
-    return;
+    message('Could not find any lint results')
+    return
   }
   let isStandardOutput = _.flow(
-    _.filter("messages"),
-    _.filter(["errorCount", 0]),
+    _.filter('messages'),
+    _.filter(['errorCount', 0]),
     _.isEmpty
-  )(lintResults);
+  )(lintResults)
 
   if (isStandardOutput) {
     if (lintResults && lintResults.length > 0) {
       fail(`Your PR has lint errors. Please fix these and commit them.
-        ${formatStandardJSLint(lintResults)}`);
+        ${formatStandardJSLint(lintResults)}`)
     }
-  } else if (_.sumBy("errorCount", lintResults) > 0) {
+  } else if (_.sumBy('errorCount', lintResults) > 0) {
     fail(`Your PR has lint errors. Please fix these and commit them.
-      ${formatLint(2)(lintResults)}`);
+      ${formatLint(2)(lintResults)}`)
   }
-};
+}
 
 let hasLintWarnings = ({ lintResults, warn, message }) => {
   if (_.isNil(lintResults)) {
-    message("Could not find any lint results");
-    return;
+    message('Could not find any lint results')
+    return
   }
-  if (_.sumBy("warningCount", lintResults) > 0) {
+  if (_.sumBy('warningCount', lintResults) > 0) {
     warn(
       `Your PR has lint warnings. Please consider fixing these.
       ${formatLint(1)(lintResults)}`
-    );
+    )
   }
-};
+}
 
 module.exports = {
   hasLintErrors,
-  hasLintWarnings
-};
+  hasLintWarnings,
+}
