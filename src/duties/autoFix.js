a67b251ee1933a31638cd00e6af927b56fe673c9
Sean Clayton <clayton.sean@gmail.com>
2017-11-03 13:30:25 -0400

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti

--- src/duties/autoFix.js
+++ src/duties/autoFix.js
@@ -6,12 +6,17 @@ let autoFix = async ({ message }) => {
   } catch (e) {
     message('No `duti:fix` npm script found. Not autofixing this PR.')
   }
-  execSync(
-    'git commit -am "Automagically formatted by Duti!\n\nhttps://github.com/smartprocure/duti" && git push'
-  )
-  message(
-    'We were able to automatically fix some formatting issues in this PR for you!'
-  )
+  try {
+    execSync(
+      'git commit -am "Automagically formatted by Duti!\n\nhttps://github.com/smartprocure/duti" && git push'
+    )
+    message(
+      'We were able to automatically fix some formatting issues in this PR for you!'
+    )
+  } catch (e) {
+    // eslint-disable-next-line
+    console.log(e)
+  }
 }
 
 module.exports = {
