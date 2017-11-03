f9a44ff1206e2b8ac3e91a2142df16b7ad202f01
Sean Clayton <clayton.sean@gmail.com>
2017-11-03 13:27:32 -0400

    Automagically formatted by Duti!
    https://github.com/smartprocure/duti

--- src/duties/autoFix.js
+++ src/duties/autoFix.js
@@ -3,15 +3,15 @@ let { execSync } = require('child_process')
 let autoFix = async ({ message }) => {
   try {
     execSync('npm run duti:fix')
-    execSync(
-      'git commit -am "Automagically formatted by Duti!\n\nhttps://github.com/smartprocure/duti" && git push'
-    )
-    message(
-      'We were able to automatically fix some formatting issues in this PR for you!'
-    )
   } catch (e) {
     message('No `duti:fix` npm script found. Not autofixing this PR.')
   }
+  execSync(
+    'git commit -am "Automagically formatted by Duti!\n\nhttps://github.com/smartprocure/duti" && git push'
+  )
+  message(
+    'We were able to automatically fix some formatting issues in this PR for you!'
+  )
 }
 
 module.exports = {
