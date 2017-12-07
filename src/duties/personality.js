1b5753ae21b696a1d428352daebaf4e9e448ba4a
Sean Clayton <clayton.sean@gmail.com>
2017-12-07 14:04:04 -0500

    Add custom config option

--- src/duties/personality.js
+++ src/duties/personality.js
@@ -4,7 +4,7 @@ let diffThreshold = ({ markdown, danger, config }) => {
   let diffTotal =
     (_.get('github.pr.additions', danger) || 0) +
     (_.get('github.pr.deletions', danger) || 0)
-  if (diffTotal >= _.get('prNetChangeThreshold', config))
+  if (diffTotal >= _.get('personalityNetChangeThreshold', config))
     markdown(
       `![kill all humans](https://media.licdn.com/mpr/mpr/shrinknp_800_800/p/2/005/0b3/059/36a09a3.jpg)`
     )
--- src/duties/personality.spec.js
+++ src/duties/personality.spec.js
@@ -6,7 +6,7 @@ describe('personality', () => {
   it('adds a message if a diff threshold has been met', () => {
     let markdown = jest.fn()
     let danger = { github: { pr: { additions: 250, deletions: 250 } } }
-    let config = { prNetChangeThreshold: 500 }
+    let config = { personalityNetChangeThreshold: 500 }
     personality.diffThreshold({ markdown, danger, config })
 
     expect(markdown).toHaveBeenCalled()
@@ -14,7 +14,7 @@ describe('personality', () => {
   it(`doesn't add a message if the threshold has not been met`, () => {
     let markdown = jest.fn()
     let danger = { github: { pr: { additions: 0, deletions: 0 } } }
-    let config = { prNetChangeThreshold: 500 }
+    let config = { personalityNetChangeThreshold: 500 }
     personality.diffThreshold({ markdown, danger, config })
 
     expect(markdown).not.toHaveBeenCalled()
