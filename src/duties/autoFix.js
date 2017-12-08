ca414a48b85677abf8a4ca30b75fa8d9b96f58be
Sean Clayton <clayton.sean@gmail.com>
2017-12-08 16:49:24 -0500

    Remove console.log

--- src/duties/autoFix.js
+++ src/duties/autoFix.js
@@ -8,10 +8,6 @@ let autoFix = async ({ message, warn, markdown, config }) => {
   try {
     execSync('npm run duti:fix')
     let out = _.head(await execP('git diff --shortstat'))
-    // eslint-disable-next-line
-    console.log({
-      out,
-    })
     let reg = /(\d+) insertions?[\D]*(\d+) deletions?/g
     let vals = reg.exec(out)
     if (vals && vals.length === 3) {
